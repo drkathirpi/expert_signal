964c817fb10f4d876642c6c78a3648ef89b5f89f
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-03-30 23:57:00 +0100

    Lecrae

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,11 @@
 //opening and closing the menu bar
 
-
+window.onscroll = ()=> {
+    var scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (scrollCalculate / height) * 100;
+    document.querySelector(".progress-bar").style.width = scrolled + "%";
+}
 
 $(document).ready(function(){
 
@@ -9,3 +14,5 @@ $(document).ready(function(){
     });
   
   });
+
+
