4dab218959fc60d3f645b2b9c30eb1060100298b
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-04-01 12:42:41 +0100

    one second

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-//opening and closing the menu bar
-
 window.onscroll = ()=> {
     var scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -7,12 +5,28 @@ window.onscroll = ()=> {
     document.querySelector(".progress-bar").style.width = scrolled + "%";
 }
 
-$(document).ready(function(){
 
-    $('.menu-bar i').click(()=>{
+$(document).ready(function(){
+    $("#sticker").sticky({topSpacing:0, center:true});
+    $('#sticker').on('sticky-start', ()=>{
+        $('#sticker').css({'background':'white', "padding":"5px auto"});
+        $('#sticker .logo h3 span:nth-child(2)').css('color','#8624c3','')
+    })
+    $('#sticker').on('sticky-end', ()=>{
+        $('#sticker').css('background','transparent');
+        $('#sticker .logo h3 span:nth-child(2)').css('color','whitesmoke')
+    })
+     
+    $('.menu-bar').click(()=>{
         $('.menu').slideToggle();
-    });
-  
+
+        if(document.querySelector('.menu-bar i').className === 'ri-menu-line'){
+            document.querySelector('.menu-bar i').className = 'ri-close-line';
+        }
+        else{
+           document.querySelector('.menu-bar i').className ="ri-menu-line";
+        }
+    })
   });
 
 
