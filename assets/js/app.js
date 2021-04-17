3d6e9819a6cce9bfccd133410b93121cfafea211
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-04-17 09:25:17 +0100

    Refactors to the design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,32 @@
-window.onscroll = ()=> {
-    var scrollCalculate = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (scrollCalculate / height) * 100;
-    document.querySelector(".progress-bar").style.width = scrolled + "%";
-}
-
 
 $(document).ready(function(){
-    $("#sticker").sticky({topSpacing:0, center:true});
+    $("#sticker").sticky({topSpacing:0, center:true, widthFromWrapper:true, zIndex:24});
     $('#sticker').on('sticky-start', ()=>{
         $('#sticker').css({'background':'white', "padding":"5px auto"});
-        $('#sticker .logo h3 span:nth-child(2)').css('color','#8624c3','')
+        $('#sticker .logo h3 span:nth-child(2)').css('color','purple','');
+        $('#sticker .menu-bar i').css('color', "black")
     })
     $('#sticker').on('sticky-end', ()=>{
+        $('#sticker .menu-bar i').css('color', "")
         $('#sticker').css('background','transparent');
         $('#sticker .logo h3 span:nth-child(2)').css('color','whitesmoke')
     })
      
     $('.menu-bar').click(()=>{
-        $('.menu').slideToggle();
+       document.querySelector('.menu').classList.toggle('hide');
 
         if(document.querySelector('.menu-bar i').className === 'ri-menu-line'){
             document.querySelector('.menu-bar i').className = 'ri-close-line';
+            document.querySelector('.menu-bar i').style.color="black";
         }
         else{
            document.querySelector('.menu-bar i').className ="ri-menu-line";
+           document.querySelector('.menu-bar i').style.color='white';
         }
     })
 
     $('#readmebtn').click(()=>{
-         $('.read_more').slideToggle();
+         $('.read_more').slideToggle('slow');
     })
   });
 
