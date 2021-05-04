126b6988759e57423ad5a8dd67d3f387e5062fa5
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-05-04 11:24:11 +0100

    Deploys

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC2Ge6Bkxgh7O7UlWr4g_sHcrCRsYKo3yo",
+    authDomain: "expert-signals-website.firebaseapp.com",
+    projectId: "expert-signals-website",
+    storageBucket: "expert-signals-website.appspot.com",
+    messagingSenderId: "475756382920",
+    appId: "1:475756382920:web:32c8c011cc0a744621b3fd",
+    measurementId: "G-91GNBL8VXW"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+//Jquery web animations
 
 $(document).ready(function(){
     $("#sticker").sticky({topSpacing:0, center:true, widthFromWrapper:true, zIndex:24});
