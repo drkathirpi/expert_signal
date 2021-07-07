8b1f062f37e439d495a72a6df7f824dd0526d78f
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-07 12:45:17 +0100

    enter

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { config, engine } = require("express-edge");
 const port = process.env.PORT || 7000;
 app.set('views', __dirname + '/views');
 app.use(engine);
-app.use(express.static('./public'));
+app.use(express.static('public'));
 app.use('/', routes)
 
 app.listen(port, ()=>{
--- routes/index-routes.js
+++ routes/index-routes.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 const router = express.Router()
 const landPage =  require("../controllers/homepage");
-
+app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
