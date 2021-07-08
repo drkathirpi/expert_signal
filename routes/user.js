95798eac53f62e769fc541581fc36fef2b3691d2
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-08 01:34:03 +0100

    creating user account

--- routes/user.js
+++ routes/user.js
@@ -1,2 +1,10 @@
 const express = require("express");
-const {check, validationResult}
+const {check, validationResult} =  require('express-validator');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+const User = require('../database/models/User')
+
+router.post('/user/create', 
+     check("username", "Please Enter a Valid Username")
+)
