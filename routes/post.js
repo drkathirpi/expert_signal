2d2398b52cfd2badff3858f7cc1fca3430e8dba4
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-19 17:46:35 +0100

    creating post model

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const createSignal =  require('../models/Post');
+const CryptoSignal =  require('../models/Post');
 const router = express.Router();
 const {encrypt, decrypt} = require('../config/encrypt')
 
@@ -8,12 +8,29 @@ router.post('/create/signal', (req,res)=>{
    const{name , signals} = req.body;
    let errors = [];
    if(!signals || !name){
-       errors.push({Msg: "Make sure both input fields are filled"})
-   }else{
-    var enc_signals = encrypt(signals);
+   errors.push({Msg: "Make sure both input fields are filled"});
    }
-   
-   
-})
+   else{
+      signals = encrypt(signals);
+      CryptoSignal.findOne({signals : signals}, (err,data)=>{
+        if(err) {
+          console.log(err)
+        } 
+        if(data){
+         errors.push({msg: "User Already Exists"})
+         }
+         else{
+            const crypto_signal = new CryptoSignal({
+               name : name,
+               signal: signals
+            })
+            crypto_signal.save((err)=>{
+               if (err) throw err;
+            })
+         }
+      })
 
+
+   }
+})
 module.exports = router;
