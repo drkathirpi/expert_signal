505c934defa406275e6f77d9416822e5bcb27364
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-21 16:41:22 +0100

    done and dusted

--- routes/admin.js
+++ routes/admin.js
@@ -2,17 +2,21 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const router = express.Router();
+const Post = require('../models/Post')
+const {encrypt, decrypt} = require('../config/encrypt')
+
 
 router.get('/auth/admin/expertsignals', (req,res)=>{
     res.render('admin_login');
 })
 
-router.post('/auth/admin/expertsignals', (req,res)=>{
+router.post('/auth/admin/expertsignals',async (req,res)=>{
+    const posts = await Post.find({});
     if(!req.body.password === 'Beans'){
         res.send('Go back idiot')
     }
     else{
-        res.render('admin')
+        res.render('admin', {posts, decrypt})
     }
 })
 
