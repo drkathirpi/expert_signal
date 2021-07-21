caedd8c74444e17ab33d959fb2649e3117a41d1b
Jesse Akoh <akohgodwinjesse@gmail.com>
2021-07-21 18:23:48 +0100

    client side javascript

--- public/js/admin_fuc.js
+++ public/js/admin_fuc.js
@@ -1,10 +1,8 @@
-const deleteBox = document.querySelector('.delete');
+const deleteBox = document.querySelectorAll('.delete');
 
-deleteBox.addEventListener('click', function() {
-    fetch('/delete/signal', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        }
+deleteBox.forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        e.preventDefault();
+        console.log(e.target.parentElement.parentElement.parentElement.querySelector('.text p').innerHTML);
     })
 })
